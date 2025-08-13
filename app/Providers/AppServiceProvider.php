f531d58c2427f9bc2b64282355fc1d19cc141540
Alexey <blockstar2k@gmail.com>
2025-08-13 14:37:20 +0300

    log viewer access

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Auth\ConcreteTelegramSessionGuard;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,14 @@ class AppServiceProvider extends ServiceProvider
         Auth::extend('telegram-session', function ($app, $name, array $config) {
             $provider = Auth::createUserProvider($config['provider']);
             return new ConcreteTelegramSessionGuard($provider, $app['request']);
+        });
+
+        // Доступ к логам только для админа
+        LogViewer::auth(function ($request) {
+            return $request->user()
+                && in_array($request->user()->email, [
+                    'admin@email.net',
+                ]);
         });
     }
 }
